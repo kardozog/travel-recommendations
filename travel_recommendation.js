3885e50f6931e0cc656198176d56bbb0bb8727d8
kardem gunday <kardemgunday@gmail.com>
2025-04-02 09:22:55 -0400

    functions

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -7,25 +7,20 @@ function searchCondition() {
   fetch('travel_recommendation_api.json')
   .then(response => response.json())
   .then(data => 
-    const condition = data.conditions.find(item => item.name.toLowerCase() === input);
+    let condition = data.find(item => item.name.toLowerCase() === input);
 
-    if (condition) {
-      const symptoms = condition.symptoms.join(', ');
-      const prevention = condition.prevention.join(', ');
-      const treatment = condition.treatment;
-
-      resultDiv.innerHTML += `<h2>${condition.name}</h2>`;
-      resultDiv.innerHTML += `<img src="${condition.imagesrc}" alt="hjh">`;
-
-      resultDiv.innerHTML += `<p><strong>Symptoms:</strong> ${symptoms}</p>`;
-      resultDiv.innerHTML += `<p><strong>Prevention:</strong> ${prevention}</p>`;
-      resultDiv.innerHTML += `<p><strong>Treatment:</strong> ${treatment}</p>`;
-    } else {
-      resultDiv.innerHTML = 'Condition not found.';
-    }
-  }))
+    if (condition === input) {
+      console.log(data);
+    } 
+    
+    
+    else {
+      resultDiv.innerHTML = 'Destination not found.';
+    });
 
 
   .catch(error => {
     console.error('Error:', error);
-    });
+    }); 
+}
+btnSearch.addEventListener('click', searchCondition); 
